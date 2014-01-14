c28f5f006098c55eb98fb44425cce56577f494df
Richard Russon (flatcap) <richard.russon@gmail.com>
2014-01-14 19:34:07 +0000

    flatten classes

--- examplewindow.cpp
+++ examplewindow.cpp
@@ -1,46 +1,48 @@
 #include <iostream>
 #include "examplewindow.h"
 
-ExampleWindow::ExampleWindow() :
-	m_VBox(Gtk::ORIENTATION_VERTICAL)
+ExampleWindow::ExampleWindow()
 {
 	set_default_size(500, 200);
 
 	add(m_TreeView);
 
+	m_Columns.add (m_col_id);
+	m_Columns.add (m_col_name);
+	m_Columns.add (m_col_number);
+	m_Columns.add (m_col_percentage);
+
 	m_refTreeModel = Gtk::ListStore::create(m_Columns);
 	m_TreeView.set_model(m_refTreeModel);
 
 	Gtk::TreeModel::Row row = *(m_refTreeModel->append());
-	row[m_Columns.m_col_id] = 1;
-	row[m_Columns.m_col_name] = "Billy Bob";
-	row[m_Columns.m_col_number] = 10;
-	row[m_Columns.m_col_percentage] = 15;
+	row[m_col_id] = 1;
+	row[m_col_name] = "Billy Bob";
+	row[m_col_number] = 10;
+	row[m_col_percentage] = 15;
 
 	row = *(m_refTreeModel->append());
-	row[m_Columns.m_col_id] = 2;
-	row[m_Columns.m_col_name] = "Joey Jojo";
-	row[m_Columns.m_col_number] = 20;
-	row[m_Columns.m_col_percentage] = 40;
+	row[m_col_id] = 2;
+	row[m_col_name] = "Joey Jojo";
+	row[m_col_number] = 20;
+	row[m_col_percentage] = 40;
 
 	row = *(m_refTreeModel->append());
-	row[m_Columns.m_col_id] = 3;
-	row[m_Columns.m_col_name] = "Rob McRoberts";
-	row[m_Columns.m_col_number] = 30;
-	row[m_Columns.m_col_percentage] = 70;
+	row[m_col_id] = 3;
+	row[m_col_name] = "Rob McRoberts";
+	row[m_col_number] = 30;
+	row[m_col_percentage] = 70;
 
-	m_TreeView.append_column("ID",   m_Columns.m_col_id);
-	m_TreeView.append_column("Name", m_Columns.m_col_name);
+	m_TreeView.append_column("ID",   m_col_id);
+	m_TreeView.append_column("Name", m_col_name);
 
-	m_TreeView.append_column_numeric("Formatted number", m_Columns.m_col_number, "%010d" /* 10 digits, using leading zeroes. */);
+	m_TreeView.append_column_numeric("Formatted number", m_col_number, "%010d");
 
-	//Display a progress bar instead of a decimal number:
 	Gtk::CellRendererProgress* cell = Gtk::manage(new Gtk::CellRendererProgress);
 	int cols_count = m_TreeView.append_column("Some percentage", *cell);
 	Gtk::TreeViewColumn* pColumn = m_TreeView.get_column(cols_count - 1);
-	if(pColumn)
-	{
-		pColumn->add_attribute(cell->property_value(), m_Columns.m_col_percentage);
+	if(pColumn) {
+		pColumn->add_attribute(cell->property_value(), m_col_percentage);
 	}
 
 	show_all_children();
--- examplewindow.h
+++ examplewindow.h
@@ -10,26 +10,12 @@ public:
 	~ExampleWindow() = default;
 
 protected:
-	class ModelColumns : public Gtk::TreeModel::ColumnRecord
-	{
-	public:
-		ModelColumns()
-		{
-			add (m_col_id);
-			add (m_col_name);
-			add (m_col_number);
-			add (m_col_percentage);
-		}
+	Gtk::TreeModelColumn<int>           m_col_id;
+	Gtk::TreeModelColumn<Glib::ustring> m_col_name;
+	Gtk::TreeModelColumn<short>         m_col_number;
+	Gtk::TreeModelColumn<int>           m_col_percentage;
 
-		Gtk::TreeModelColumn<int>           m_col_id;
-		Gtk::TreeModelColumn<Glib::ustring> m_col_name;
-		Gtk::TreeModelColumn<short>         m_col_number;
-		Gtk::TreeModelColumn<int>           m_col_percentage;
-	};
-
-	ModelColumns m_Columns;
-
-	Gtk::Box m_VBox;
+	Gtk::TreeModel::ColumnRecord m_Columns;
 
 	Gtk::TreeView m_TreeView;
 	Glib::RefPtr<Gtk::ListStore> m_refTreeModel;
