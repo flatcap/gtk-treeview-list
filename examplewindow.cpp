1e26b5bc7d5428762f9b1178d6d6e3d4c0fa689f
Richard Russon (flatcap) <richard.russon@gmail.com>
2014-01-16 02:11:39 +0000

    try to add totals row

--- examplewindow.cpp
+++ examplewindow.cpp
@@ -14,11 +14,20 @@ ExampleWindow::ExampleWindow (void)
 
 	m_TreeView.signal_row_activated().connect (sigc::mem_fun (*this, &ExampleWindow::on_row_activated));
 	m_TreeView.set_activate_on_single_click (true);
+	m_TreeView.set_rules_hint (true);
 
 	Gtk::TreeModel::Row row;
-	row = *(m_refTreeModel->append()); row[m_col_id] = 1; row[m_col_name] = "Billy"; row[m_col_number] = 10; row[m_col_percentage] =       15; row[m_col_suffix] = "M";
-	row = *(m_refTreeModel->append()); row[m_col_id] = 2; row[m_col_name] = "Joey";  row[m_col_number] = 20; row[m_col_percentage] =     4000; row[m_col_suffix] = "WW";
-	row = *(m_refTreeModel->append()); row[m_col_id] = 3; row[m_col_name] = "Rob";   row[m_col_number] = 30; row[m_col_percentage] = 70000000; row[m_col_suffix] = "MMM";
+	row = *(m_refTreeModel->append()); row[m_col_id] = 1; row[m_col_name] = "Billy";  row[m_col_number] = 10; row[m_col_percentage] =   15; row[m_col_suffix] = "MiB";
+	row = *(m_refTreeModel->append()); row[m_col_id] = 2; row[m_col_name] = "Joey";   row[m_col_number] = 20; row[m_col_percentage] = 4000; row[m_col_suffix] = "MiB";
+	row = *(m_refTreeModel->append()); row[m_col_id] = 3; row[m_col_name] = "Rob";    row[m_col_number] = 30; row[m_col_percentage] =  700; row[m_col_suffix] = "MiB";
+
+#if 0
+	row = *(m_refTreeModel->append());
+	row = *(m_refTreeModel->append()); row[m_col_id] = 5; row[m_col_name] = "Totals"; row[m_col_number] = 60; row[m_col_percentage] = 4714; row[m_col_suffix] = "MiB";
+
+	Gtk::CellRendererText *cell_renderer_text = dynamic_cast<Gtk::CellRendererText*>(get_column(0)->get_first_cell_renderer());
+	get_column(0)->add_attribute(cell_renderer_text->property_foreground(), treeview_detail_columns .text_color);
+#endif
 
 	m_TreeView.append_column         ("ID",     m_col_id);			// 0
 	m_TreeView.append_column         ("Name",   m_col_name);		// 1
@@ -32,7 +41,12 @@ ExampleWindow::ExampleWindow (void)
 	pColumn->get_first_cell()->set_alignment(1.0, 0.5);
 	pColumn->set_alignment (1.0);
 
-	set_default_size (400, 150);
+#if 0
+	std::vector<Gtk::CellRenderer*> v = pColumn->get_cells();
+	std::cout << "column has " << v.size() << " renderers" << std::endl;
+#endif
+
+	set_default_size (200, 100);
 	add (m_Box);
 	m_Box.pack_start (m_TreeView, false, false);
 	show_all_children();
@@ -46,9 +60,11 @@ void ExampleWindow::on_row_activated (const Gtk::TreeModel::Path& path, Gtk::Tre
 		Gtk::TreeModel::Row row = *iter;
 		std::cout << "Row activated: Name=" << row[m_col_name] << std::endl;
 
+#if 0
 		row[m_col_number]     = row[m_col_number]     + 100;
 		row[m_col_percentage] = row[m_col_percentage] * 2;
 		row[m_col_suffix]     = row[m_col_suffix]     + "W";
+#endif
 	}
 }
 
